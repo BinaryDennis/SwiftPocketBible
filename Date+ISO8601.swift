cebbbc6e40f35465d8acb4809306cef3b74cd8db
Dennis Charmington <dennis.k@mac.com>
2017-04-04 16:29:22 +0200

    Create Date+ISO8601.swift

---
+++ Date+ISO8601.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+extension Date {
+    
+    static let iso8601Formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+        return formatter
+    }()
+    
+    var iso8601: String {
+        return Date.iso8601Formatter.string(from: self)
+    }
+}
+
+extension String {
+    var dateFromISO8601: Date? {
+        return Date.iso8601Formatter.date(from: self)
+    }
+}
