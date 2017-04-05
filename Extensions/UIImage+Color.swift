5333d0232d0d2fde3e53522dba4b10913a3c6253
Dennis Charmington <dennis.k@mac.com>
2017-04-05 16:54:55 +0200

    Update UIImage+Color.swift

--- Extensions/UIImage+Color.swift
+++ Extensions/UIImage+Color.swift
@@ -1,19 +1,19 @@
 extension UIImage {
 
-    class func imageFrom(color: UIColor, size: CGSize = CGSize(width: 1, height: 1) ) -> UIImage {
+     class func imageFrom(color: UIColor, size: CGSize = CGSize(width: 1, height: 1) ) -> UIImage {
         let rect = CGRect(origin: CGPoint(x: 0, y:0), size: size)
 
         UIGraphicsBeginImageContext(rect.size)
+        defer {
+            UIGraphicsEndImageContext()
+        }
 
         if let context = UIGraphicsGetCurrentContext() {
             context.setFillColor(color.cgColor)
             context.fill(rect)
         }
 
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        return image!
+        return UIGraphicsGetImageFromCurrentImageContext()!
     }
 
 }
