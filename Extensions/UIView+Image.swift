9637920446df2691367efbcbd85106e4ed60cd49
Dennis Charmington <dennis.k@mac.com>
2017-04-05 16:20:32 +0200

    Update UIView+Image.swift

--- Extensions/UIView+Image.swift
+++ Extensions/UIView+Image.swift
@@ -1,7 +1,11 @@
 extension UIView {
-    public func getSnapshotImage() -> UIImage? {
+    public func getSnapshotImage(withBackgroundColor bgColor: CGColor = UIColor.white.cgColor) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(self.bounds.size, self.isOpaque, 0)
-        self.drawHierarchy(in: self.bounds, afterScreenUpdates: false)
+        if let ctx = UIGraphicsGetCurrentContext() {
+            ctx.setFillColor(bgColor)
+            ctx.fill(self.bounds)
+        }
+        self.drawHierarchy(in: self.bounds, afterScreenUpdates: true)
         let snapshotImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return snapshotImage
