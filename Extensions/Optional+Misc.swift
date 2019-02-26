5c48cddc5d81546be149fc3c6c9fa71db5fa01fa
Dennis Charmington <dennis@charmington.se>
2019-02-26 08:56:17 +0100

    Update and rename Optional+Require.swift to Optional+Misc.swift

--- Extensions/Optional+Misc.swift
+++ Extensions/Optional+Misc.swift
@@ -36,3 +36,26 @@ public extension Optional {
         return unwrapped
     }
 }
+
+
+pubilc extension Optional where Wrapped == String {
+    var isNilOrEmpty: Bool {
+        switch self {
+        case let string?:
+            return string.isEmpty
+        case nil:
+            return true
+        }
+    }
+}
+
+pubilc extension Optional where Wrapped: Collection {
+    var isNilOrEmpty: Bool {
+        switch self {
+        case let collection?:
+            return collection.isEmpty
+        case nil:
+            return true
+        }
+    }
+}
